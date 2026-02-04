486b48e9b3fbac7944f926c60569fee7e135c23b
David Armstrong <darmstrong4@crest-l001.cm.cluster>
2026-02-04 14:11:30 -0600

    uodate figures

--- 12_publication_figures/scripts/12_publication_figures.R
+++ 12_publication_figures/scripts/12_publication_figures.R
@@ -2177,7 +2177,7 @@ pca_summer_combined <- ggplot(summer_combined,
                                aes(x = PC1_scaled, y = PC2_scaled, 
                                    color = Treatment, shape = Organism)) +
     geom_point(size = 5, alpha = 0.8, stroke = 1.5) +
-    stat_ellipse(aes(group = Treatment), level = 0.95, linetype = "dashed", linewidth = 0.8) +
+    stat_ellipse(aes(group = Treatment), level = 0.9, linetype = "dashed", linewidth = 0.8) +
     geom_text_repel(aes(label = sample),
                     size = 3,
                     fontface = "bold",
